da855427a7d5ff805b1ca74963b4ebbf20e9730a
vieiragranzoto <vieiragranzoto@gmail.com>
2023-05-09 14:00:01 -0300

    DELETE import

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -2,7 +2,6 @@ package br.com.alura.mvc.mudi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 //@EnableCaching
 @SpringBootApplication
